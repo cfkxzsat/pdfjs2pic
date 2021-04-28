07a34b0417ed23b935ea894cd240162da93a84da
zhouxu <zhouxu@jinghang.ai>
2021-04-28 10:50:47 +0800

    mod：生成pdf全部页

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ class PDF2png {
   async run(pdfEndpoint, page, pngPath) {
     await this.page.goto(`file:${path.join(__dirname, `index.html?pdfEndpoint=${pdfEndpoint}&page=${page}&scale=${this.scale}`)}`);
     await this.page.waitForSelector('#endIdentifier');
+    const ehArr = await this.page.$$('#canvas-wrapper>canvas');
 
-    const eh = await this.page.$('#the-canvas');
-
-    const base64 = await eh.evaluate((node) => node.toDataURL());
-    fs.writeFileSync(pngPath, base64.replace(/^data:image\/png;base64,/, ""), {
-        encoding: 'base64'
-    })
+    for (let i = 0; i < ehArr.length; i++) {
+      const eh = ehArr[i];
+      const base64 = await eh.evaluate((node) => node.toDataURL());
+      fs.writeFile(`${i + 1}.png`, base64.replace(/^data:image\/png;base64,/, ""), {
+          encoding: 'base64'
+      }, (err) => console.error(err))
+    }
   }
 
   async stop() {
@@ -27,5 +29,19 @@ class PDF2png {
   }
 
 }
+
+(async() => {
+
+  const p = new PDF2png();
+  await p.init();
+  await p.run(`pastPaper/teacher-try-out/Mark-scheme/furtherMathematics-edexcel/Edexcel_2014_Jan_WFM01-01_ms.pdf`);
+  // await p.run(`pastPaper/Edexcel/Economics/2014.1/Question-paper/Edexcel_2014_Jan_6ECA3-01_qp.pdf`, 1, `2.png`);
+  // await p.run(`pastPaper/teacher-try-out/Mark-scheme/furtherMathematics-edexcel/Edexcel_2014_Jan_WFM01-01_ms.pdf`, 3, `3.png`);
+
+
+//   await p.stop();
+
+})()
+
 
 module.exports = PDF2png;
