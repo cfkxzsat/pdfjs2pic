fcfb2f037dc4faf4229236449aec8f042a183602
zhouxu <zhouxu@jinghang.ai>
2021-05-11 16:22:26 +0800

    feat：success log & constructor

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@ const PDF2png = require('../index');
 
 (async() => {
 
-  const p = new PDF2png();
+  const p = new PDF2png({headless: false});
   await p.init();
   const p1 = p.run(`https://alg-question-1255803335.cos.ap-beijing.myqcloud.com/dev/a_level/pdf/a92ebf515f74ca0ee11b075a9e35540d.pdf`, __dirname);
   const p2 = p.run(`https://alg-question-1255803335.cos.ap-beijing.myqcloud.com/dev/a_level/pdf/5b3aa4fdf89d10e73067fb49f6f5dedc.pdf`, process.cwd(), '2,4');
--- index.js
+++ index.js
@@ -3,24 +3,33 @@ const fs = require('fs');
 const path = require('path');
 
 class PDF2png {
-  async init(scale) {
+  constructor({scale, headless}) {
     this.scale = scale ?? 8;
+    this.headless = headless ?? true;
+  }
+  async init() {
     this.browser = await puppeteer.launch({
-      headless: false
+      headless: this.headless
     });
   }
   async run(pdfEndpoint, pngDirPath, pageNo) {
     const newPage = await this.browser.newPage();
     await newPage.goto(`file:${path.join(__dirname, `index.html?pdfEndpoint=${pdfEndpoint}&pageNo=${pageNo}&scale=${this.scale}`)}`);
-    await newPage.waitForSelector('#endIdentifier', {timeout: 0});
+    await newPage.waitForSelector('#endIdentifier', {timeout: 0});// 默认30s超时
     const ehArr = await newPage.$$('#canvas-wrapper>canvas');
 
     for (let i = 0; i < ehArr.length; i++) {
       const eh = ehArr[i];
       const {base64, pageNo} = await eh.evaluate((node) => ({base64: node.toDataURL(), pageNo: node.getAttribute('pageno')}));
-      fs.writeFile(path.resolve(pngDirPath, `${pageNo}.png`), base64.replace(/^data:image\/png;base64,/, ""), {
+      const pngPath = path.resolve(pngDirPath, `${pageNo}.png`);
+      fs.writeFile(pngPath, base64.replace(/^data:image\/png;base64,/, ""), {
           encoding: 'base64'
-      }, (err) => {if(err)console.error(err)})
+      }, (err) => {
+        if(err) {
+          console.error(err)
+        }else {
+          console.log(`[pdfjs2pic done]: ${pngPath}`)
+        }})
     }
     await newPage.close();
   }
